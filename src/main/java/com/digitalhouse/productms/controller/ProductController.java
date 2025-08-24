37a01f240923a65acd4426713b606d40bb0fbe55
Mateo <m473o.v@gmail.com>
2025-08-24 15:22:20 -0300

    feat: microservices with gateway

--- src/main/java/com/digitalhouse/productms/controller/ProductController.java
+++ src/main/java/com/digitalhouse/productms/controller/ProductController.java
@@ -2,8 +2,10 @@ package com.digitalhouse.productms.controller;
 
 import com.digitalhouse.productms.dto.UserResponseDTO;
 import com.digitalhouse.productms.service.UserFeignServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,11 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers(@RequestHeader("X-Request-from") String requestFrom) {
+        System.out.println("Request from: " + requestFrom);
+        if (requestFrom == null || !requestFrom.equals("gateway")) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         List<UserResponseDTO> users = this.userFeignService.getAllUsers();
         return ResponseEntity.ok(users);
     }
